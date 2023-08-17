a1aa3f4e2b2b5958e486a60043a426936e2c138b
Lostervac <140098917+Lostervac@users.noreply.github.com>
2023-08-17 19:13:40 +0300

    python bot

---
+++ python bot/ayarlar.py
@@ -0,0 +1,6 @@
+#settings.py in original
+
+ayarlar = {
+    "on_taki": ">", #ön takı, prefix (maybe it won't work if we call it different here)
+    "TOKEN": "MTEzNjcxODgwMTU3Mjk5OTIwOQ.GChLtf.z4Fb6UzYa9xiMX27sKkwzgz54ZaniAzS2sSENs"
+}
